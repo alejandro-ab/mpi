18780831c244eba47c40ac9732380efd414bc9b7
Alejandro Alvarez <alejandro.alvarez@evertecinc.com>
2026-07-10 16:49:08 -0500

    refactor: standardize quotation marks in authentication indicator documentation

--- src/Messages/QueryResponseVersionTwo.php
+++ src/Messages/QueryResponseVersionTwo.php
@@ -52,15 +52,14 @@ class QueryResponseVersionTwo extends QueryResponse
 
     /**
      * Returns the indicator for the authentication from the ACS
-     *  “Y” - Successful Authentication
-     *  “A” - Successful Attempt
-     *  “N” - Failed Authentication
-     *  “U” - Unable to Authenticate
-     *  ”C” - Challenge Required
-     *  ”D” - Challenge Required
-     *  ”R” - Failed Authenticate
-     *  ”I” - Challenge Required only informative.
-     * @return string
+     *  "Y" - Successful Authentication
+     *  "A" - Successful Attempt
+     *  "N" - Failed Authentication
+     *  "U" - Unable to Authenticate
+     *  "C" - Challenge Required
+     *  "D" - Challenge Required
+     *  "R" - Failed Authenticate
+     *  "I" - Challenge Required only informative.
      */
     public function authenticated(): ?string
     {
